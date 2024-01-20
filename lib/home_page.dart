be0a53311939e74ec6bacdd2509c4fa183261397
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-19 21:43:22 -0500

    txtfiel style finish

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,27 +12,47 @@ class HomePage extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
             child: Column(
               children: [
+                Text(
+                  "MIS RECETAS",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                Divider(),
                 //estados textfiel: Enabled Focus, error disable
                 TextField(
                   decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 8, horizontal: 16),
                     labelStyle: TextStyle(color: Colors.orange),
                     hintText: "Sugerencia de nombre de ttulo",
                     filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(
-                        color: Colors.black,
-                        width: 2,
+                    fillColor: Colors.black12,
+                    prefixIcon: SvgPicture.asset(
+                      "assets/icons/type.svg",
+                      fit: BoxFit.scaleDown,
+                      colorFilter: ColorFilter.mode(
+                        Colors.black45,
+                        BlendMode.srcIn,
                       ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+
+                      // borderSide: BorderSide(
+                      //   color: Colors.black26,
+                      //   width: 1,
+                      // ),
                     ),
                     focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      borderSide: BorderSide(
-                        color: Colors.green,
-                        width: 3,
-                      ),
-                    ),
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: BorderSide.none
+                        // borderSide: BorderSide(
+                        //   color: Colors.black26,
+                        //   width: 1.5,
+                        // ),
+                        ),
                   ),
                 ),
               ],
