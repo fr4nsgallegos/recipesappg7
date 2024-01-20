aefbef61a8a42a6b5a6745b45a2e8b5fee27d1b1
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-19 19:21:01 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/recipesappg7/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.recipesappg7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:recipesappg7/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
