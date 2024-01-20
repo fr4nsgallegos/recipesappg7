6f1dd2d74171cc1385f5c19b33dcacdff43ede77
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-19 20:12:04 -0500

    textfield

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    // label: Row(
+                    //   children: [
+                    //     Icon(Icons.title),
+                    //     Text("Título"),
+                    //   ],
+                    // ),
+                    labelStyle: TextStyle(color: Colors.orange),
+                    labelText: "Ingresa el título",
+                    hintStyle: TextStyle(color: Colors.red),
+                    hintText: "Sugerencia de nombre de receta",
+                  ),
+                ),
+                // TextField(
+                //   decoration: InputDecoration(
+                //     labelText: "Ingresa el preparación",
+                //     hintText: "Sugerencia de preparación",
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
