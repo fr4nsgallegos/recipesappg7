b7824ec5de4a8eb2146b7b16c626e65be0f79477
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-20 09:54:48 -0500

    regex textfield

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -33,7 +34,13 @@ class HomePage extends StatelessWidget {
                     color: Colors.white,
                   ),
                   cursorColor: Color(0xffEDA971),
-
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(5),
+                    FilteringTextInputFormatter(
+                      RegExp(r'[0-9]'),
+                      allow: false,
+                    )
+                  ],
                   controller: titleController,
                   decoration: InputDecoration(
                     contentPadding:
