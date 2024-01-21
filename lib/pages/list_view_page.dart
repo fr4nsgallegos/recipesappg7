1ca50467178c4ed6a26ce93fb1f6a1a5975093fa
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2024-01-21 09:31:51 -0500

    init singlechildscrollvierw

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -5,8 +5,36 @@ class ListViewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 500,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: 200,
+              width: 500,
+              color: Colors.red,
+            ),
+            Container(
+              height: 200,
+              width: 500,
+              color: Colors.green,
+            ),
+            Container(
+              height: 200,
+              width: 500,
+              color: Colors.black,
+            ),
+            Container(
+              height: 200,
+              width: 500,
+              color: Colors.orange,
+            ),
+          ],
+        ),
       ),
     );
   }
